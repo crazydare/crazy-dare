28956bd57ac1e48dd065d6699be66e45810148c6
crazydare <drew@drewkamp.com>
2022-04-28 19:26:01 +0200

    adding moved message

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import Form from "./pages/Form";
 import WheelSection from "./pages/WheelSection";
 //Style and Motion
 import "./App.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   const [footerIsDisplayed, setFooterIsDisplayed] = useState(true);
 
   return (
     <div className="wrapper">
-      <Switch>
-        <Route path="/" exact>
-          <WheelSection></WheelSection>
-        </Route>
-        <Route path="/form">
-          <Form></Form>
-        </Route>
-      </Switch>
-
-      {footerIsDisplayed && (
-        <footer>
-          <Link to="/form" onClick={() => setFooterIsDisplayed(false)}>
-            Get notified of new games.
-          </Link>
-        </footer>
-      )}
+      We have moved to a new page:
+      <div className="code-block">
+        https://wheelofdares.netlify.app/custom/5658/
+      </div>
+      <a href="https://wheelofdares.netlify.app/custom/5658/">
+        <motion.button
+          whileHover={{
+            scale: 1.2,
+            transition: { type: "spring", stiffness: 600 },
+          }}
+        >
+          Visit Wheel of Dares
+        </motion.button>
+      </a>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -11,18 +11,7 @@ export const data = [
   { option: "x3" },
 ];
 
-export const results = [
-  "In the forums, go to HotStuff category, choose one of the first 10 topics and post a photo related to it. Post the link.",
-  "Take a screenshot of your phone's private photo galley and post it.",
-  "Post a photo of your hard cock covered by a sock.",
-  "Post full-body (with face) photo totally naked.",
-  "Roll two more times and complete the 2 dares.",
-  "Upload a video of you pissing and post the link. Face optional.",
-  "Send your Password to all the other 7s above you.",
-  "Post your Password",
-  "Wank on cam in the chatroom and post a screenshot.",
-  "Roll three more times and complete the 3 dares.",
-];
+export const results = ["", "", "", "", "", "", "", "", "", ""];
 
 export const textColors = [
   "#f9fafa",
